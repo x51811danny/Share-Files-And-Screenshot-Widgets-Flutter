319f74ec3579033f357135a93872bfa1229bb647
Danny Chen <dannychen@geniuswork.com.tw>
2023-06-06 12:43:53 +0800

    [chore] downgrade to flutter 1.x

--- lib/share_files_and_screenshot_widgets.dart
+++ lib/share_files_and_screenshot_widgets.dart
@@ -8,29 +8,17 @@ import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
 class ShareFilesAndScreenshotWidgets {
-  static const MethodChannel _channel =
-      const MethodChannel('channel:share_files_and_screenshot_widgets');
+  static const MethodChannel _channel = const MethodChannel('channel:share_files_and_screenshot_widgets');
 
   /// for share related text
   static void text(String title, String text, String mimeType) {
-    Map argsMap = <String, String>{
-      'title': '$title',
-      'text': '$text',
-      'mimeType': '$mimeType'
-    };
+    Map argsMap = <String, String>{'title': '$title', 'text': '$text', 'mimeType': '$mimeType'};
     _channel.invokeMethod('text', argsMap);
   }
 
   /// writing file in temporary directory
-  static Future<void> file(
-      String title, String name, List<int> bytes, String mimeType,
-      {String text = ''}) async {
-    Map argsMap = <String, String>{
-      'title': '$title',
-      'name': '$name',
-      'mimeType': '$mimeType',
-      'text': '$text'
-    };
+  static Future<void> file(String title, String name, List<int> bytes, String mimeType, {String text = ''}) async {
+    Map argsMap = <String, String>{'title': '$title', 'name': '$name', 'mimeType': '$mimeType', 'text': '$text'};
 
     final tempDir = await getTemporaryDirectory();
     final file = await new File('${tempDir.path}/$name').create();
@@ -40,9 +28,7 @@ class ShareFilesAndScreenshotWidgets {
   }
 
   /// writing files in temporary directory
-  static Future<void> files(
-      String title, Map<String, List<int>> files, String mimeType,
-      {String text = ''}) async {
+  static Future<void> files(String title, Map<String, List<int>> files, String mimeType, {String text = ''}) async {
     Map argsMap = <String, dynamic>{
       'title': '$title',
       'names': files.entries.toList().map((x) => x.key).toList(),
@@ -61,37 +47,30 @@ class ShareFilesAndScreenshotWidgets {
   }
 
   /// takes screenshot of the widget and returns Image
-  Future<Image?> takeScreenshot(
-      GlobalKey previewContainer, int originalSize) async {
+  Future<Image> takeScreenshot(GlobalKey previewContainer, int originalSize) async {
     final currentContext = previewContainer.currentContext;
     if (currentContext == null) {
       return null;
     }
-    RenderRepaintBoundary boundary =
-        currentContext.findRenderObject() as RenderRepaintBoundary;
+    RenderRepaintBoundary boundary = currentContext.findRenderObject() as RenderRepaintBoundary;
     double pixelRatio = originalSize / MediaQuery.of(currentContext).size.width;
     ui.Image image = await boundary.toImage(pixelRatio: pixelRatio);
-    ByteData? byteData =
-        await (image.toByteData(format: ui.ImageByteFormat.png));
-    Uint8List pngBytes = byteData!.buffer.asUint8List();
+    ByteData byteData = await (image.toByteData(format: ui.ImageByteFormat.png));
+    Uint8List pngBytes = byteData.buffer.asUint8List();
     return Image.memory(pngBytes.buffer.asUint8List());
   }
 
   /// takes screenshot and invokes share
-  shareScreenshot(GlobalKey previewContainer, int originalSize, String title,
-      String name, String mimeType,
-      {String text = ''}) async {
+  shareScreenshot(GlobalKey previewContainer, int originalSize, String title, String name, String mimeType, {String text = ''}) async {
     final currentContext = previewContainer.currentContext;
     if (currentContext == null) {
       return;
     }
-    RenderRepaintBoundary boundary =
-        currentContext.findRenderObject() as RenderRepaintBoundary;
+    RenderRepaintBoundary boundary = currentContext.findRenderObject() as RenderRepaintBoundary;
     double pixelRatio = originalSize / MediaQuery.of(currentContext).size.width;
     ui.Image image = await boundary.toImage(pixelRatio: pixelRatio);
-    ByteData? byteData =
-        await (image.toByteData(format: ui.ImageByteFormat.png));
-    Uint8List pngBytes = byteData!.buffer.asUint8List();
+    ByteData byteData = await (image.toByteData(format: ui.ImageByteFormat.png));
+    Uint8List pngBytes = byteData.buffer.asUint8List();
     try {
       await file(title, name, pngBytes, mimeType, text: text);
     } catch (e) {
@@ -100,8 +79,7 @@ class ShareFilesAndScreenshotWidgets {
   }
 
   /// share any type of file using this function
-  shareFile(String title, String name, Uint8List bytes, String mimeType,
-      {String text = ''}) async {
+  shareFile(String title, String name, Uint8List bytes, String mimeType, {String text = ''}) async {
     try {
       await file(title, name, bytes, mimeType, text: text);
     } catch (e) {
